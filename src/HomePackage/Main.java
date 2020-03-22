546bb5bae457b06486b242cfbdc0086772d8af91
kimb0078 <kimberlysuaris29@hotmail.com>
2020-03-22 02:57:04 +0100

    Del 2 - under construction

--- src/HomePackage/Main.java
+++ src/HomePackage/Main.java
@@ -15,15 +15,6 @@ public class Main {
                 duckRace.printDucks();
             }
 
-            /*System.out.println("Ducks after round 1");
-            duckRace.nextRound(9);
-            duckRace.printDucks();
-
-            System.out.println("Round 2");
-            duckRace.nextRound(8);
-            duckRace.printDucks();
-            */
-
         }
         catch (IndexOutOfBoundsException e){
             e.printStackTrace();
